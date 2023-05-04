6e9f78b3d54183269b401e3865f1f2c34b68a399
Kshitij Jande <kshitij.jande.main@gmail.com>
2023-05-04 12:36:19 +0530

    Utility method to join an array of integers

--- src/main/java/com/kshitij/abuseipdbwrapper/utils/BasicUtils.java
+++ src/main/java/com/kshitij/abuseipdbwrapper/utils/BasicUtils.java
@@ -2,6 +2,7 @@ package com.kshitij.abuseipdbwrapper.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class BasicUtils {
@@ -10,8 +11,12 @@ public class BasicUtils {
         return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse(s);
     }
 
-    public static void sendRequest() {
+    public static String joinIntArray(int[] array) {
+        return String.join(",", Arrays.stream(array).mapToObj(String::valueOf).toArray(String[]::new));
+    }
 
+    public static String joinIntArray(int[] array, String delimiter) {
+        return String.join(delimiter, Arrays.stream(array).mapToObj(String::valueOf).toArray(String[]::new));
     }
 
 }
