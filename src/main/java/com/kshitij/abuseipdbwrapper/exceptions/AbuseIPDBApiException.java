94c338f6f1b42fe193a2f13a4d454cf82b487060
Kshitij Jande <kshitij.jande.main@gmail.com>
2023-05-04 11:47:29 +0530

    Exception to show full response body

--- src/main/java/com/kshitij/abuseipdbwrapper/exceptions/AbuseIPDBApiException.java
+++ src/main/java/com/kshitij/abuseipdbwrapper/exceptions/AbuseIPDBApiException.java
@@ -15,7 +15,7 @@ public class AbuseIPDBApiException extends RuntimeException {
     private final List<String> errorDetails = new ArrayList<>();
 
     public AbuseIPDBApiException(int statusCode, String body) {
-        super("Got an error from AbuseIPDB's API.");
+        super("Got an error from AbuseIPDB's API. Full body: " + body);
         this.statusCode = statusCode;
         JsonElement jsonElement = JsonParser.parseString(body);
         this.body = jsonElement.getAsJsonObject();
